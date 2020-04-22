c00456bc7830fc53ff9d3b5bc2b672ca8742264e
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-22 19:37:26 -0300

    validações de data e mostrando os dados abaixo do form

--- FrontEnd/exercises_6-1/script.js
+++ FrontEnd/exercises_6-1/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     let state = document.querySelector('#estado');
     let curriculo = document.querySelector('#content-curriculo');
     let btnSend = document.querySelector('#send');
-
+    let btnClear = document.querySelector('#clear');
 
     let estadosBr = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
     for (estados of estadosBr) {
@@ -12,7 +12,6 @@ window.onload = function () {
         state.appendChild(opcao);
     }
 
-    
     btnSend.addEventListener('click', function (e) {
         e.preventDefault();
         validaNome()
@@ -23,13 +22,19 @@ window.onload = function () {
         validaEstado()
         validaResumo()
         validaCargo()
+        validaDescCargo()
+        validaData()
     });
+
+    btnClear.addEventListener('click', function() {
+        preventDefault();
+    })
 
     function validaNome() {
         let nome = document.querySelector('#nome');
         if (nome.value !== '') {
-            console.log(nome.value);     
-        }else {
+            apresentaDados(nome.value);
+        } else {
             alert('Digite seu nome!')
             nome.focus();
         }
@@ -37,78 +42,100 @@ window.onload = function () {
 
     function validaEmail() {
         let email = document.querySelector('#email');
-        if (email.value == '' ||email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        if (email.value == '' || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
             alert('Digite um email valido! não esqueça do @ e .');
             email.focus();
-        }else {
-            console.log(email.value);
+        } else {
+            apresentaDados(email.value);
         }
     }
 
     function validaCpf() {
         let cpf = document.querySelector('#cpf');
-        if (cpf.value == '' ) {
+        if (cpf.value == '') {
             alert('O campo deve ter numeros');
             cpf.focus();
         } else {
-            console.log(cpf.value);
+            apresentaDados(cpf.value);
         }
     }
 
     function validaEnd() {
         let end = document.querySelector('#end');
-        if (end.value == '' ) {
+        if (end.value == '') {
             alert('O campo deve ter numeros');
             end.focus();
         } else {
-            console.log(end.value);
+            apresentaDados(end.value);
         }
     }
 
     function validaCidade() {
         let cidade = document.querySelector('#city');
-        if (cidade.value == '' ) {
+        if (cidade.value == '') {
             alert('O campo deve ter numeros');
             cidade.focus();
         } else {
-            console.log(cidade.value);
+            apresentaDados(cidade.value);
         }
     }
 
     function validaEstado() {
         let state = document.querySelector('#estado');
-        if (state.value == 'selecione' ) {
+        if (state.value == 'selecione') {
             alert('Escolha um Estado');
             state.focus();
         } else {
-            console.log(state.value);
+            apresentaDados(state.value);
         }
     }
 
     function validaResumo() {
         let resumo = document.querySelector('#resumo');
-        if (resumo.value == '' ) {
-            alert('O campo deve ter numeros');
+        if (resumo.value == '') {
+            alert('Queremos te conhecer mais');
             resumo.focus();
         } else {
-            console.log(resumo.value);
+            apresentaDados(resumo.value);
         }
     }
-    
+
     function validaCargo() {
         let cargo = document.querySelector('#cargo');
-        if (cargo.value == '' ) {
-            alert('O campo deve ter numeros');
+        if (cargo.value == '') {
+            alert('Digite um cargo');
             cargo.focus();
         } else {
-            console.log(cargo.value);
+            apresentaDados(cargo.value);
         }
     }
+    function validaDescCargo() {
+        let cargoDesc = document.querySelector('#desc-cargo');
+        if (cargoDesc.value == '') {
+            alert('Descreva brevemente seu cargo');
+            cargo.focus();
+        } else {
+            apresentaDados(cargoDesc.value);
+        }
+    }
+
     function validaData() {
         let data = document.querySelector('#data');
-        
+        let dia = data.value.substr(0, 2);
+        let mes = data.value.substr(3, 2);
+        let ano = data.value.substr(6, 4);
+        if (data.value !== '' || ((dia > 0 && dia <= 31) && (mes > 0 && mes <= 12) && (ano.length === 4 && ano >= 0))) {
+          
+            apresentaDados(data.value);
+        } else {
+            alert('o formato da data é 00/00/0000');
+            data.focus(); 
+        }
+    }
+
+    function apresentaDados(element) {
+        let dado = document.createElement('p');
+        dado.innerHTML = element;
+        curriculo.appendChild(dado);
     }
 }
-
-
-
