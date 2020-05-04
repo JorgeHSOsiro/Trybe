a86eaa514a4ff0c6ad7a2076bf971737b1cdbce6
Jorge Osiro <jojimurakabe@gmail.com>
2020-05-04 20:24:41 -0300

    exercicios 5 a 8

---
+++ EcmaScript6/exercises_8-2/exercicio-7.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const getValuesByNumber = (object, number) => {
+      let valores = Object.values(object);
+      let valor = valores[number];
+      console.log(valor);
+  }
+  getValuesByNumber(lesson1,1)
+  
