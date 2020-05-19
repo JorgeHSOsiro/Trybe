b81ba45a142e34c37dceadeb50909ee59a6d07d8
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-05-19 12:10:24 -0300

    exercicio 6

--- EcmaScript6/exercises_10-1/exercicio-6.js
+++ EcmaScript6/exercises_10-1/exercicio-6.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 function myFizzBuzz(num) {
   if (typeof num !== 'number') return false;
   if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
@@ -8,9 +6,4 @@ function myFizzBuzz(num) {
   return num;
 }
 
-// implemente seus testes aqui
-assert.equal(myFizzBuzz(15),'fizzbuzz');
-assert.equal(myFizzBuzz(3),'fizz');
-assert.equal(myFizzBuzz(5),'buzz');
-assert.equal(myFizzBuzz(13),13);
-assert.equal(myFizzBuzz('string'),false);
+module.exports = myFizzBuzz;
