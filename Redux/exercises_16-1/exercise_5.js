0d1618d9ce498d5a34fdcdc6f0ef313efad1c641
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-11 19:12:46 -0300

    exercise 5

--- Redux/exercises_16-1/exercise_5.js
+++ Redux/exercises_16-1/exercise_5.js
@@ -1,34 +1,43 @@
+// change code below this line
+const LOGIN = 'LOGIN'
+const LOGOUT = 'LOGOUT'
+// change code above this line
+
 const defaultState = {
   authenticated: false
 };
 
 const authReducer = (state = defaultState, action) => {
-  // change code below this line
-  switch(action.type) {
-    case 'LOGIN':
+
+  switch (action.type) {
+
+    case LOGIN:
       return {
-        authenticated: true,
+        authenticated: true
       }
-    case 'LOGOUT':
+
+    case LOGOUT:
       return {
-        authenticated: false,
+        authenticated: false
       }
-    default: 
+
+    default:
       return state;
+
   }
-  // change code above this line
+
 };
 
 const store = Redux.createStore(authReducer);
 
 const loginUser = () => {
   return {
-    type: 'LOGIN'
+    type: LOGIN
   }
 };
 
 const logoutUser = () => {
   return {
-    type: 'LOGOUT'
+    type: LOGOUT
   }
 };
